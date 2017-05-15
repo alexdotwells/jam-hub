98da25a29248d3285faf1b56ac7d107cf747b39d
Alexander J Wells <ajwells99@gmail.com>
2017-05-15 00:25:16 -0500

    Added WebApi base project

---
+++ src/JamHub/JH.WebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace JH.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ src/JamHub/JH.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JH.WebApi.WebApiApplication" Language="C#" %>
