ccbf8e65c14d868d2eef0eccb63fd6d61bf991b0
Alexander J Wells <ajwells99@gmail.com>
2017-05-18 23:49:58 -0500

    Added new user registration

---
+++ src/JamHub/JH.Client/app/mainCtrl.js
@@ -0,0 +1,50 @@
+﻿(function () {
+    "use strict";
+
+    angular
+        .module("jamManagement")
+        .controller("MainCtrl",
+                    ["userAccount",
+                        "currentUser",
+                        MainCtrl]);
+
+    function MainCtrl(userAccount, currentUser) {
+        var vm = this;
+        vm.isLoggedIn = function () {
+            return currentUser.getProfile().isLoggedIn;
+        };
+        vm.message = '';
+        vm.userData = {
+            userName: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+        };
+
+        vm.registerUser = function () {
+            vm.userData.confirmPassword = vm.userData.password;
+
+            userAccount.registration.registerUser(vm.userData,
+                function (data) {
+                    vm.confirmPassword = "";
+                    vm.message = "... Registration successful";
+                    vm.login();
+                },
+                function (response) {
+                    vm.isLoggedIn = false;
+                    vm.message = response.statusText + "\r\n";
+                    if (response.data.exceptionMessage)
+                        vm.message += response.data.exceptionMessage;
+
+                    // Validation errors
+                    if (response.data.modelState) {
+                        for (var key in response.data.modelState) {
+                            vm.message += response.data.modelState[key] + "\r\n";
+                        }
+                    }
+                });
+        }
+
+        
+    }
+})();
