cfe6514f81f56a9100417e42c8ddac979e643cf0
Alexander J Wells <ajwells99@gmail.com>
2017-06-03 23:50:41 -0500

    Added Jam resource

---
+++ src/JamHub/JH.Client/Common/jamResource.js
@@ -0,0 +1,30 @@
+﻿(function () {
+    "use strict";
+
+    angular
+        .module("common.services")
+        .factory("jamResource",
+                ["$resource",
+                 "appSettings",
+                 "currentUser",
+                    jamResource])
+
+    function jamResource($resource, appSettings, currentUser) {
+        return $resource(appSettings.serverPath + "/api/jams/:id", null,
+            {
+                'get': {
+                    headers: { 'Authorization': 'Bearer ' + currentUser.getProfile().token }
+                },
+
+                'save': {
+                    headers: { 'Authorization': 'Bearer ' + currentUser.getProfile().token }
+                },
+
+                'update': {
+                    method: 'PUT',
+                    headers: { 'Authorization': 'Bearer ' + currentUser.getProfile().token }
+                }
+            });
+    }
+}());
+
