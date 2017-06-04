5b080afb6f23d350cafc578576f24d986cf428cc
Alexander J Wells <ajwells99@gmail.com>
2017-06-04 00:02:36 -0500

    Added Jam model

---
+++ src/JamHub/JH.WebApi/Models/Jam.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JH.WebApi.Models
+{
+    public class Jam
+    {
+        public string Description { get; set; }
+        public decimal Rating { get; set; }
+        public string JamCode { get; set; }
+        public int JamId { get; set; }
+        public string JamName { get; set; }
+        public DateTime AdddedDate { get; set; }
+    }
+}
