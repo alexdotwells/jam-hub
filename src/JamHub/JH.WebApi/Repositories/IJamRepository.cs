edf717f2261fc296827b43b62658b46b35e53fb0
Alexander J Wells <ajwells99@gmail.com>
2017-06-05 23:18:20 -0500

    Added interface for Jam Repositoy

---
+++ src/JamHub/JH.WebApi/Repositories/IJamRepository.cs
@@ -0,0 +1,14 @@
+﻿using JH.WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JH.WebApi.Repositories
+{
+    internal interface IJamRepository
+    {
+        List<Jam> Retrieve();
+        Jam Save(Jam jam);
+    }
+}
